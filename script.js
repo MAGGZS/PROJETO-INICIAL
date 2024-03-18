537a86e3e384dccdcaf7fbd30a763e7beb1d3464
MAGGZS <magdielbr1@gmail.com>
2024-03-18 09:56:04 -0300

    formulário

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click",alertar);
+var nome = document.getElementById("nome");
+
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
+
+var telefone = document.getElementById("telefone");
+
+var cep = document.getElementById("cep");
+
+var logradouro = document.getElementById("logradouro");
+
+var numero = document.getElementById("numero");
+
+var complemento = document.getElementById("complemento");
+
+var bairro = document.getElementById("bairro");
+
+var cidade = document.getElementById("cidade");
+
+var estado = document.getElementById("estado");
 
 function alertar(){
-    alert("Voce clicou no botao!!!");
+   // alert("Voce clicou no botao!!!");
+   saida.innerText = "nome: " + " " + nome.value +
+                            "\n E-mail: " + " " + email.value +
+                            "\n Telefone: " + " " + telefone.value +
+                            "\n cep: " + " " + cep.value +
+                            "\n logradouro: " + " " + logradouro.value +
+                            "\n numero: " + " " + numero.value +
+                            "\n complemento: " + " " + complemento.value+
+                            "\n Bairoo: " + " " + bairro.value+
+                            "\n Cidade: " + " " + cidade.value+
+                            "\n Estado: " + " " + estado.value;
+
 }
